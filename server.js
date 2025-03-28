99fe6294188e91c163d92b5dafb97779597558be
sujit <sujit1505v@gmail.com>
2025-03-28 14:50:39 +0530

    🛠️ server.js -> Updated CORS configuration comments.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    // origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
